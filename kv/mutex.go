79c9ee3bc6765a34c070afd0a7bc51c8b6cd2ce5
Anton Konovalov <anton.konovalov@ncloudtech.ru>
2016-06-16 16:50:42 +0300

    added  mutex test kv by channels

--- kv/mutex.go
+++ kv/mutex.go
@@ -1,6 +1,8 @@
 package kv
 
-import "sync"
+import (
+	"sync"
+)
 
 type KV struct {
 	l sync.Mutex
@@ -10,6 +12,60 @@ type KV struct {
 type KVR struct {
 	l sync.RWMutex
 	s map[string]interface{}
+}
+
+type d struct {
+	k string
+	v interface{}
+}
+
+func newSpy(key string) *spy {
+	return &spy{
+		k: key,
+		v: make(chan interface{}),
+	}
+}
+
+type spy struct {
+	k string
+	v chan interface{}
+}
+
+type KVC struct {
+	in  chan *d
+	s   map[string]interface{}
+	out chan *spy
+}
+
+func NewKVC() *KVC {
+	store := &KVC{
+		s:   make(map[string]interface{}, 0),
+		in:  make(chan *d),
+		out: make(chan *spy),
+	}
+
+	go func() {
+		for {
+			select {
+			case data := <-store.in:
+				store.s[data.k] = data.v
+			case s := <-store.out:
+				s.v <- store.s[s.k]
+			}
+		}
+	}()
+
+	return store
+}
+
+func (k *KVC) Set(key string, val interface{}) {
+	k.in <- &d{key, val}
+}
+
+func (k *KVC) Get(key string) interface{} {
+	transfer := newSpy(key)
+	k.out <- transfer
+	return <-transfer.v
 }
 
 func (k *KV) Set(key string, val interface{}) {
@@ -39,3 +95,5 @@ func (k *KVR) Get(key string) interface{} {
 var KVStore = &KV{s: make(map[string]interface{}, 0)}
 
 var KVRStore = &KVR{s: make(map[string]interface{}, 0)}
+
+var KVChannelStore = NewKVC()
--- kv/mutex_test.go
+++ kv/mutex_test.go
@@ -5,13 +5,90 @@ import (
 	"testing"
 )
 
+type KeyValueStore interface {
+	Set(key string, v interface{})
+	Get(key string) interface{}
+}
+
+var fake = &struct{ somefield string }{`ALOXA`}
+
+func generateData(count int, store KeyValueStore) {
+	for i := 0; i < count; i++ {
+		store.Set(uuid.New(), fake)
+	}
+}
+
+func TestKVC_Get(t *testing.T) {
+	obj := &struct{ somefield string }{`ALOXA`}
+
+	KVChannelStore.Set(`key`, obj)
+	KVChannelStore.Set(`key-1`, `data-1`)
+	KVChannelStore.Set(`key-2`, `data-2`)
+
+	data := KVChannelStore.Get(`key`)
+	if data == nil {
+		t.Fatalf(`method get must return data by exists key`)
+	}
+
+	data = KVChannelStore.Get(`key-1`)
+	if data == nil || data.(string) != `data-1` {
+		t.Fatalf(`method get must return data by exists key`)
+	}
+
+	data = KVChannelStore.Get(`key-2`)
+	if data == nil || data.(string) != `data-2` {
+		t.Fatalf(`method get must return data by exists key`)
+	}
+}
+
 func BenchmarkKV_Set(b *testing.B) {
 	obj := &struct{ somefield string }{`ALOXA`}
+	generateData(1000000, KVStore)
 
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			KVStore.Set(uuid.New(), obj)
+			KVStore.Set(`somekey`, obj)
+		}
+	})
+}
+
+func BenchmarkKV_Get(b *testing.B) {
+	obj := &struct{ somefield string }{`ALOXA`}
+	key := uuid.New()
+	generateData(1000000, KVRStore)
+	KVStore.Set(key, obj)
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			KVStore.Get(key)
+		}
+	})
+}
+
+func BenchmarkKV_Channel_Set(b *testing.B) {
+	obj := &struct{ somefield string }{`ALOXA`}
+	generateData(1000000, KVChannelStore)
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			KVChannelStore.Set(`somekey`, obj)
+		}
+	})
+}
+
+func BenchmarkKV_Channel_Get(b *testing.B) {
+	obj := &struct{ somefield string }{`ALOXA`}
+	key := uuid.New()
+	generateData(1000000, KVChannelStore)
+	KVChannelStore.Set(key, obj)
+
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			KVChannelStore.Get(key)
 		}
 	})
 }
@@ -19,6 +96,7 @@ func BenchmarkKV_Set(b *testing.B) {
 func BenchmarkKVR_Get(b *testing.B) {
 	obj := &struct{ somefield string }{`ALOXA`}
 	key := uuid.New()
+	generateData(1000000, KVRStore)
 	KVRStore.Set(key, obj)
 
 	b.ResetTimer()
@@ -29,15 +107,12 @@ func BenchmarkKVR_Get(b *testing.B) {
 	})
 }
 
-func BenchmarkKV_Get(b *testing.B) {
+func BenchmarkMap_Successively_Set(b *testing.B) {
 	obj := &struct{ somefield string }{`ALOXA`}
-	key := uuid.New()
-	KVStore.Set(key, obj)
+	store := make(map[int]interface{})
 
 	b.ResetTimer()
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			KVStore.Get(key)
-		}
-	})
+	for i := 0; i < b.N; i++ {
+		store[i] = obj
+	}
 }
